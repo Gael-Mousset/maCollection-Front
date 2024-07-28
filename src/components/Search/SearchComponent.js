ed1b1367fbe84063486932e10c35809b20f1d78d
Gael Mousset <gaelmousset@outlook.fr>
2024-07-28 10:14:44 +0200

    Test /proxy

--- src/components/Search/SearchComponent.js
+++ src/components/Search/SearchComponent.js
@@ -11,7 +11,7 @@ const SearchComponent = ({ addToCollection }) => {
   const searchGames = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.get(`http://localhost:5000/proxy`, {
+      const response = await axios.get(`/proxy`, {
         params: {
           name: query,
         },
